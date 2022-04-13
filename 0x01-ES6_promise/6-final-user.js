035ffbb700e2a3413f630620106cff7826a3a747
Capotedev <carolina.capote@imagineapps.co>
2022-04-13 18:28:52 -0500

    ADD- functio nto call 2 functions and return array with the data returned in the promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,10 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const user = await signUpUser(firstName, lastName).then((user) => ({
-    status: 'fullfilled', value: user,
+    status: 'fulfilled', value: user,
   }));
   const file = await uploadPhoto(fileName).catch((error) => ({
     status: 'rejected', value: error.toString(),
   }));
-  return Promise.resolve([user, file]);
+  return [user, file];
 }
