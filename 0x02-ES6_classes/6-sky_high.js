087d3321fa6b5d6ade4013548ff415cb5f58d2e3
Capotedev <carolina.capote@imagineapps.co>
2022-04-14 22:34:13 -0500

    FIX return value

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,7 +3,6 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._sqft = sqft;
     this._floors = floors;
   }
 
@@ -19,6 +18,6 @@ export default class SkyHighBuilding extends Building {
    * Return a warning message
    */
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors.`;
   }
 }
