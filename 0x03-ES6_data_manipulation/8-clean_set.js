f1faecac95ea1ac35c9ad5d09d1c6f2473807aee
Capotedev <carolina.capote@imagineapps.co>
2022-04-19 18:51:14 -0500

    change the return value

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,11 +5,10 @@ export default function cleanSet(set, startString) {
   if (startString.length === 0) return '';
   let result = '';
   set.forEach((element) => {
-    if (startString.length < element.length) {
-      if (element.slice(0, startString.length) === startString) {
-        result = `${result.concat(element.slice(startString.length, element.length))}-`;
-      }
+    if (element.slice(0, startString.length) === startString) {
+      result = `${result.concat(element.slice(startString.length, element.length))}-`;
     }
   });
-  return result.slice(0, -1);
+  if (result.length !== 0) return result.slice(0, -1);
+  return result;
 }
