978e2c7b71993de43fb43a8a3c0707ab67d2f0cc
Capotedev <carolina.capote@imagineapps.co>
2022-04-19 18:36:24 -0500

    fix result

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,11 +2,13 @@
  *returns a string of all the set values that start with a specific string
  */
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || typeof startString !== 'string') return '';
+  if (startString.length === 0) return '';
   let result = '';
   set.forEach((element) => {
-    if (element.slice(0, startString.length) === startString) {
-      result = `${result.concat(element.slice(startString.length, element.length))}-`;
+    if (startString.length < element.length) {
+      if (element.slice(0, startString.length) === startString) {
+        result = `${result.concat(element.slice(startString.length, element.length))}-`;
+      }
     }
   });
   return result.slice(0, -1);
