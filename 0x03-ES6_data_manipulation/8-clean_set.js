7315e232339cbee80c29fb3f216ec72efdd51221
Capotedev <carolina.capote@imagineapps.co>
2022-04-19 17:45:24 -0500

    returns a string with all the values that contains the strString at the beginning

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@
  *returns a string of all the set values that start with a specific string
  */
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) return '';
+  if (startString.length === 0 || typeof startString !== 'string') return '';
   let result = '';
   set.forEach((element) => {
     if (element.slice(0, startString.length) === startString) {
