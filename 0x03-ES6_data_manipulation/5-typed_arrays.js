8cf4ca3e1cc44d14a92b1522d63db41dbe97d62b
Capotedev <carolina.capote@imagineapps.co>
2022-04-19 15:02:46 -0500

    create new arrayBuffer and change value in a specific position

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position > length - 1) {
+    throw Error('Position outside range');
+  }
   const newArray = new ArrayBuffer(length);
   const int8View = new Int8Array(newArray);
   int8View[position] = value;
