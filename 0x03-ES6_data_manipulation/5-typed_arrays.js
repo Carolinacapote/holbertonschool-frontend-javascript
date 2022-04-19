88f6fb8e02fd0ce76a48846048e7a1728617ee21
Capotedev <carolina.capote@imagineapps.co>
2022-04-19 15:14:59 -0500

    return new dataView of an arrayBuffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,5 +5,5 @@ export default function createInt8TypedArray(length, position, value) {
   const newArray = new ArrayBuffer(length);
   const int8View = new Int8Array(newArray);
   int8View[position] = value;
-  return newArray;
+  return new DataView(newArray, 0);
 }
