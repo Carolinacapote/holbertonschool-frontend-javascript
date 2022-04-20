41c389bcf167aee98b02d7ab223a032ad09507f5
Capotedev <carolina.capote@imagineapps.co>
2022-04-19 19:09:32 -0500

    validate type of variable

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,10 +5,11 @@ export default function cleanSet(set, startString) {
   if (startString.length === 0) return '';
   let result = '';
   set.forEach((element) => {
-    if (element.slice(0, startString.length) === startString) {
-      result = `${result.concat(element.slice(startString.length, element.length))}-`;
+    if (typeof element === 'string') {
+      if (element.slice(0, startString.length) === startString) {
+        result = `${result.concat(element.slice(startString.length, element.length))}-`;
+      }
     }
   });
-  if (result.length !== 0) return result.slice(0, -1);
-  return result;
+  return result.slice(0, -1);
 }
