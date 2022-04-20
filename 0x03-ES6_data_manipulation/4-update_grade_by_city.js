a01178c3920e746492948423ab2e8015836b7dc4
Capotedev <carolina.capote@imagineapps.co>
2022-04-19 19:57:11 -0500

    filter() and map() combined

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,6 @@
 export default function updateStudentGradeByCity(studentsList, city, gradesList) {
   const students = (studentsList.filter((student) => student.location === city).map((student) => {
     const [grades] = gradesList.filter((obj) => obj.studentId === student.id);
-    console.log('aqui esta grades', grades);
     return { ...student, grade: grades ? grades.grade : 'N/A' };
   }));
   return students;
